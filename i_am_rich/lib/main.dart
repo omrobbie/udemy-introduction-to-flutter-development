50595cfd9e02793948df99adcab7b688fa8504c6
omrobbie <putu.roby@yahoo.com>
2020-09-21 16:38:43 +0800

    set image from network

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -8,6 +8,9 @@ void main() {
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Image(
+          image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+        ),
       ),
     ),
   );
