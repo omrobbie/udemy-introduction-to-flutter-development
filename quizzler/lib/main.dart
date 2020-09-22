1f2d6f6a5bbb1795c97febfd01bf3853b2e3ef4c
omrobbie <putu.roby@yahoo.com>
2020-09-22 23:23:45 +0800

    add padding to all

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -34,44 +34,54 @@ class _QuizPageState extends State<QuizPage> {
       children: [
         Expanded(
           flex: 5,
-          child: Center(
-            child: Text(
-              'This is where the question text will go',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32.0,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
               ),
             ),
           ),
         ),
         Expanded(
-          child: FlatButton(
-            color: Colors.green,
-            child: Text(
-              'True',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32.0,
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
               ),
+              onPressed: () {
+                print('True button pressed!');
+              },
             ),
-            onPressed: () {
-              print('True button pressed!');
-            },
           ),
         ),
         Expanded(
-          child: FlatButton(
-            color: Colors.red,
-            child: Text(
-              'False',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32.0,
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
               ),
+              onPressed: () {
+                print('False button pressed!');
+              },
             ),
-            onPressed: () {
-              print('True button pressed!');
-            },
           ),
         ),
       ],
