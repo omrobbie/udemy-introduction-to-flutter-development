b918041f458084c3ac841169fa6aa5473860e084
omrobbie <putu.roby@yahoo.com>
2020-09-22 22:33:38 +0800

    create another tone button

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -6,9 +7,9 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
-  void playSound(int number) {
+  void playSound(int soundNumber) {
     final player = AudioCache();
-    player.play('note$number.wav');
+    player.play('note$soundNumber.wav');
   }
 
   @override
@@ -25,6 +26,48 @@ class XylophoneApp extends StatelessWidget {
                   color: Colors.red,
                   onPressed: () {
                     playSound(1);
+                  },
+                ),
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.teal,
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.deepPurple,
+                  onPressed: () {
+                    playSound(7);
                   },
                 ),
               ],
