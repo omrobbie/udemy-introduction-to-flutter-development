8a4a69c3f0230d736faaf34dd0fb7607c14c8c0d
omrobbie <putu.roby@yahoo.com>
2020-09-22 22:31:16 +0800

    play sound with function

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -6,19 +6,28 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int number) {
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              child: Text('Click me!'),
-              onPressed: () {
-                print('You have been click the button!');
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FlatButton(
+                  height: 80.0,
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
+              ],
             ),
           ),
         ),
