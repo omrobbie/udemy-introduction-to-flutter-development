2b62580eec810920c28ee4b57dd6a4e01374a837
omrobbie <putu.roby@yahoo.com>
2020-09-22 12:35:35 +0800

    add another text

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -29,6 +29,16 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ],
           ),
         ),
