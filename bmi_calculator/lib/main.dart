5aa7cc71db806cdfbdac9dbabb2a80f291da5590
omrobbie <putu.roby@yahoo.com>
2020-10-25 10:45:35 +0800

    refactor: theme data using copyWith

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -8,13 +8,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         primaryColor: Color(0xFF0A0E21),
-        accentColor: Colors.purple,
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.white)
-        ),
       ),
       home: InputPage(),
     );
