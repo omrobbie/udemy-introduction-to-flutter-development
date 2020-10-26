0e2f21babc24df14b8335c455aa19e21580c5cf3
omrobbie <putu.roby@yahoo.com>
2020-10-26 17:07:49 +0800

    result page layout

--- bmi_calculator/lib/results_page.dart
+++ bmi_calculator/lib/results_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
@@ -11,11 +12,40 @@ class ResultsPage extends StatelessWidget {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Text(
               'Your Result',
               style: kTitleTextStyle,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMITextStyle,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Your BMI result is quite low, you should eat more!',
+                      style: kBodyTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
